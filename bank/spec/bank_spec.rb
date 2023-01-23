577d36bfc4c6642aaab4114a770f4a7bde7219ef
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2023-01-23 14:07:11 +0000

    Bank unit test: can deposit OR withdraw once, and check balance

--- bank/spec/bank_spec.rb
+++ bank/spec/bank_spec.rb
@@ -9,4 +9,18 @@ RSpec.describe Bank do
     expect(bank.print_statement).to eq "date || credit || debit || balance"
   end
 
+  it "can deposit money once and check balance for that deposit value" do
+    bank = Bank.new
+    transaction = double :transaction, amount: 1, date: "01/01/2023"
+    bank.add_transaction(transaction)
+    expect(bank.check_balance).to eq 1.00
+  end
+
+  it "can withdraw money once and check balance for that withdraw value" do
+    bank = Bank.new
+    transaction = double :transaction, amount: -1, date: "01/01/2023"
+    bank.add_transaction(transaction)
+    expect(bank.check_balance).to eq -1.00
+  end
+
 end
