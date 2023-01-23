d62ce4caecd7192e842e52b0c6508ece7d1d2c38
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2023-01-23 16:44:52 +0000

    Date mocked in transaction unit test

--- bank/lib/transaction.rb
+++ bank/lib/transaction.rb
@@ -1,2 +1,15 @@
-class Transaction < Struct.new(:amount, :date)
+class Transaction
+  def initialize(amount, date)
+    @amount = amount
+    @date = date
+  end
+
+  def amount
+    return @amount
+  end
+
+  def date
+    formatted_date = @date.today.to_s
+    return "#{formatted_date[8..9]}/#{formatted_date[5..6]}/#{formatted_date[0..3]}"
+  end
 end
--- bank/spec/transaction_spec.rb
+++ bank/spec/transaction_spec.rb
@@ -3,20 +3,25 @@ require 'transaction'
 RSpec.describe Transaction do
   context 'initializes with a transaction amount and date' do
     it 'initializes as an instance of the class' do
-      transaction = Transaction.new(1, '01/01/2023')
+      fake_date = double :date
+      transaction = Transaction.new(1, fake_date)
       expect(transaction.is_a?(Transaction)).to eq true
     end
 
     it 'is a deposit, with positive amount' do
-      transaction = Transaction.new(1, '01/01/2023')
+      fake_date = double :date
+      transaction = Transaction.new(1, fake_date)
+      expect(fake_date).to receive(:today).and_return("2023-01-23")
       expect(transaction.amount).to eq 1
-      expect(transaction.date).to eq '01/01/2023'
+      expect(transaction.date).to eq '23/01/2023'
     end
 
     it 'is a withdrawl, with negative amount' do
-      transaction = Transaction.new(-1, '05/01/2023')
+      fake_date = double :date
+      transaction = Transaction.new(-1, fake_date)
+      expect(fake_date).to receive(:today).and_return("2023-01-23")
       expect(transaction.amount).to eq(-1)
-      expect(transaction.date).to eq '05/01/2023'
+      expect(transaction.date).to eq '23/01/2023'
     end
   end
 end
