ee51286b0d1e2aa5bf3cc77be7cfabb5f931682e
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2023-01-23 15:46:52 +0000

    Refactored to remove the private method

--- bank/lib/bank.rb
+++ bank/lib/bank.rb
@@ -2,7 +2,6 @@ class Bank
   def initialize
     @balance = 0.00
     @transactions = []
-    @statement = ''
   end
 
   def check_balance
@@ -17,23 +16,15 @@ class Bank
 
   def format_transaction(transaction)
     @transactions << if transaction.amount.positive?
-                       "#{transaction.date} || #{transaction.amount}.00 || || #{@balance}0"
+                       "#{transaction.date} || #{transaction.amount}.00 || || #{@balance}0\n"
                      else
-                       "#{transaction.date} || || #{-transaction.amount}.00 || #{@balance}0"
+                       "#{transaction.date} || || #{-transaction.amount}.00 || #{@balance}0\n"
                      end
   end
 
   def print_statement
-    format_statement
-    @statement[0..-2]
+    @transactions << "date || credit || debit || balance\n"
+    return @transactions.reverse.join()[0..-2]
   end
 
-  private
-
-  def format_statement
-    @transactions << 'date || credit || debit || balance'
-    @transactions.reverse.each do |transaction|
-      @statement += "#{transaction}\n"
-    end
-  end
 end
