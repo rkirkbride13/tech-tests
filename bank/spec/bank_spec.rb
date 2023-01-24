428660a02fe5380144c5308b5cd25af86578a820
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2023-01-24 10:52:12 +0000

    Context added to tests and descriptions tweaked

--- bank/spec/bank_spec.rb
+++ bank/spec/bank_spec.rb
@@ -1,110 +1,130 @@
 require 'bank'
 
 RSpec.describe Bank do
-  it 'initializes with an account balance of 0.00 and an empty transaction array' do
-    bank = Bank.new
-    expect(bank.is_a?(Bank)).to eq true
-    expect(bank.check_balance).to eq 0.00
-    expect(bank.print_statement).to eq 'date || credit || debit || balance'
+  context "when initialized" do
+    it 'has an account balance of 0.00 and an empty transaction array' do
+      bank = Bank.new
+      expect(bank.is_a?(Bank)).to eq true
+      expect(bank.check_balance).to eq 0.00
+      expect(bank.print_statement).to eq 'date || credit || debit || balance'
+    end
   end
 
-  it 'can deposit money once and check balance for that deposit value' do
-    bank = Bank.new
-    transaction = double :transaction, amount: 1, date: '01/01/2023'
-    bank.add_transaction(transaction)
-    expect(bank.check_balance).to eq 1.00
+  context "when adding a single deposit/withdrawal transaction" do
+    it 'has a positive balance if its a deposit' do
+      bank = Bank.new
+      transaction = double :transaction, amount: 1, date: '01/01/2023'
+      bank.add_transaction(transaction)
+      expect(bank.check_balance).to eq 1.00
+    end
+
+    it 'has a balance less than the initial deposit value' do
+      bank = Bank.new
+      transaction1 = double :transaction, amount: 10, date: '01/01/2023'
+      transaction2 = double :transaction, amount: -1, date: '01/01/2023'
+      bank.add_transaction(transaction1)
+      bank.add_transaction(transaction2)
+      expect(bank.check_balance).to eq 9.00
+    end
   end
 
-  it 'can withdraw money once and check balance for that withdraw value' do
+  context "several deposits / withdrawals can be made and the balance is checked" do 
+    it 'can calculate the balance after several deposits' do
+      bank = Bank.new
+      transaction1 = double :transaction, amount: 1, date: '01/01/2023'
+      transaction2 = double :transaction, amount: 2, date: '01/01/2023'
+      transaction3 = double :transaction, amount: 9, date: '01/01/2023'
+      bank.add_transaction(transaction1)
+      bank.add_transaction(transaction2)
+      bank.add_transaction(transaction3)
+      expect(bank.check_balance).to eq 12.00
+    end
+
+    it 'can calculate the balance after several withdrawals' do
+      bank = Bank.new
+      transaction1 = double :transaction, amount: 20, date: '01/01/2023'
+      transaction2 = double :transaction, amount: -2, date: '01/01/2023'
+      transaction3 = double :transaction, amount: -5, date: '01/01/2023'
+      transaction4 = double :transaction, amount: -11, date: '01/01/2023'
+      bank.add_transaction(transaction1)
+      bank.add_transaction(transaction2)
+      bank.add_transaction(transaction3)
+      bank.add_transaction(transaction4)
+      expect(bank.check_balance).to eq 2.00
+    end
+
+    it 'can calculate the balance after the example transaction sequence' do
+      bank = Bank.new
+      transaction1 = double :transaction, amount: 1000, date: '01/01/2023'
+      transaction2 = double :transaction, amount: 2000, date: '01/01/2023'
+      transaction3 = double :transaction, amount: -500, date: '01/01/2023'
+      bank.add_transaction(transaction1)
+      bank.add_transaction(transaction2)
+      bank.add_transaction(transaction3)
+      expect(bank.check_balance).to eq 2500.00
+    end
+  end
+
+  context "printing a statement after a single deposit / withdrawal" do
+    it 'prints after a single deposit' do
+      bank = Bank.new
+      transaction = double :transaction, amount: 1, date: '01/01/2023'
+      bank.add_transaction(transaction)
+      expect(bank.print_statement).to eq "date || credit || debit || balance\n01/01/2023 || 1.00 || || 1.00"
+    end
+
+    it 'prints after a single withdrawal' do
+      bank = Bank.new
+      transaction1 = double :transaction, amount: 10, date: '01/01/2023'
+      transaction2 = double :transaction, amount: -1, date: '01/01/2023'
+      bank.add_transaction(transaction1)
+      bank.add_transaction(transaction2)
+      expect(bank.print_statement).to eq "date || credit || debit || balance\n01/01/2023 || || 1.00 || 9.00\n01/01/2023 || 10.00 || || 10.00"
+    end
+  end
+
+  context "several deposits / withdrawals can be made and the statement printed" do
+    it 'prints after a multiple deposits' do
+      bank = Bank.new
+      transaction1 = double :transaction, amount: 1, date: '01/01/2023'
+      transaction2 = double :transaction, amount: 2, date: '02/01/2023'
+      bank.add_transaction(transaction1)
+      bank.add_transaction(transaction2)
+      expect(bank.print_statement).to eq "date || credit || debit || balance\n02/01/2023 || 2.00 || || 3.00\n01/01/2023 || 1.00 || || 1.00"
+    end
+
+    it 'prints after a multiple withdrawls' do
+      bank = Bank.new
+      transaction1 = double :transaction, amount: 45, date: '01/01/2023'
+      transaction2 = double :transaction, amount: -10, date: '01/01/2023'
+      transaction3 = double :transaction, amount: -25, date: '02/01/2023'
+      bank.add_transaction(transaction1)
+      bank.add_transaction(transaction2)
+      bank.add_transaction(transaction3)
+      expect(bank.print_statement).to eq "date || credit || debit || balance\n02/01/2023 || || 25.00 || 10.00\n01/01/2023 || || 10.00 || 35.00\n01/01/2023 || 45.00 || || 45.00"
+    end
+
+    it 'prints the example transaction sequence' do
+      bank = Bank.new
+      transaction1 = double :transaction, amount: 1000, date: '10/01/2023'
+      transaction2 = double :transaction, amount: 2000, date: '13/01/2023'
+      transaction3 = double :transaction, amount: -500, date: '14/01/2023'
+      transaction4 = double :transaction, amount: -700, date: '15/01/2023'
+      bank.add_transaction(transaction1)
+      bank.add_transaction(transaction2)
+      bank.add_transaction(transaction3)
+      bank.add_transaction(transaction4)
+      expect(bank.print_statement).to eq "date || credit || debit || balance\n15/01/2023 || || 700.00 || 1800.00\n14/01/2023 || || 500.00 || 2500.00\n13/01/2023 || 2000.00 || || 3000.00\n10/01/2023 || 1000.00 || || 1000.00"
+    end
+  end
+
+  it 'raises error if there are insufficient funds' do
     bank = Bank.new
-    transaction1 = double :transaction, amount: 10, date: '01/01/2023'
-    transaction2 = double :transaction, amount: -1, date: '01/01/2023'
+    fake_date = double :date
+    transaction1 = double :transaction, amount: 10, date: '10/01/2023'
     bank.add_transaction(transaction1)
-    bank.add_transaction(transaction2)
-    expect(bank.check_balance).to eq 9.00
+    transaction2 = double :transaction, amount: -100, date: '13/01/2023'
+    expect { bank.add_transaction(transaction2) }.to raise_error 'Insufficienet funds'
   end
 
-  it 'can deposit money several times and check balance for total balance value' do
-    bank = Bank.new
-    transaction1 = double :transaction, amount: 1, date: '01/01/2023'
-    transaction2 = double :transaction, amount: 2, date: '01/01/2023'
-    transaction3 = double :transaction, amount: 9, date: '01/01/2023'
-    bank.add_transaction(transaction1)
-    bank.add_transaction(transaction2)
-    bank.add_transaction(transaction3)
-    expect(bank.check_balance).to eq 12.00
-  end
-
-  it 'can withdraw money several times and check balance for total balance value' do
-    bank = Bank.new
-    transaction1 = double :transaction, amount: 20, date: '01/01/2023'
-    transaction2 = double :transaction, amount: -2, date: '01/01/2023'
-    transaction3 = double :transaction, amount: -5, date: '01/01/2023'
-    transaction4 = double :transaction, amount: -11, date: '01/01/2023'
-    bank.add_transaction(transaction1)
-    bank.add_transaction(transaction2)
-    bank.add_transaction(transaction3)
-    bank.add_transaction(transaction4)
-    expect(bank.check_balance).to eq 2.00
-  end
-
-  it 'can deposit and withdraw money and check balance for the total balance values' do
-    bank = Bank.new
-    transaction1 = double :transaction, amount: 1000, date: '01/01/2023'
-    transaction2 = double :transaction, amount: 2000, date: '01/01/2023'
-    transaction3 = double :transaction, amount: -500, date: '01/01/2023'
-    bank.add_transaction(transaction1)
-    bank.add_transaction(transaction2)
-    bank.add_transaction(transaction3)
-    expect(bank.check_balance).to eq 2500.00
-  end
-
-  it 'can print a statement after a single deposit' do
-    bank = Bank.new
-    transaction = double :transaction, amount: 1, date: '01/01/2023'
-    bank.add_transaction(transaction)
-    expect(bank.print_statement).to eq "date || credit || debit || balance\n01/01/2023 || 1.00 || || 1.00"
-  end
-
-  it 'can print a statement after a single withdrawl' do
-    bank = Bank.new
-    transaction1 = double :transaction, amount: 10, date: '01/01/2023'
-    transaction2 = double :transaction, amount: -1, date: '01/01/2023'
-    bank.add_transaction(transaction1)
-    bank.add_transaction(transaction2)
-    expect(bank.print_statement).to eq "date || credit || debit || balance\n01/01/2023 || || 1.00 || 9.00\n01/01/2023 || 10.00 || || 10.00"
-  end
-
-  it 'can print a statement after a multiple deposits' do
-    bank = Bank.new
-    transaction1 = double :transaction, amount: 1, date: '01/01/2023'
-    transaction2 = double :transaction, amount: 2, date: '02/01/2023'
-    bank.add_transaction(transaction1)
-    bank.add_transaction(transaction2)
-    expect(bank.print_statement).to eq "date || credit || debit || balance\n02/01/2023 || 2.00 || || 3.00\n01/01/2023 || 1.00 || || 1.00"
-  end
-
-  it 'can print a statement after a multiple withdrawls' do
-    bank = Bank.new
-    transaction1 = double :transaction, amount: 45, date: '01/01/2023'
-    transaction2 = double :transaction, amount: -10, date: '01/01/2023'
-    transaction3 = double :transaction, amount: -25, date: '02/01/2023'
-    bank.add_transaction(transaction1)
-    bank.add_transaction(transaction2)
-    bank.add_transaction(transaction3)
-    expect(bank.print_statement).to eq "date || credit || debit || balance\n02/01/2023 || || 25.00 || 10.00\n01/01/2023 || || 10.00 || 35.00\n01/01/2023 || 45.00 || || 45.00"
-  end
-
-  it 'can print a statement after several deposits and withdrawls' do
-    bank = Bank.new
-    transaction1 = double :transaction, amount: 1000, date: '10/01/2023'
-    transaction2 = double :transaction, amount: 2000, date: '13/01/2023'
-    transaction3 = double :transaction, amount: -500, date: '14/01/2023'
-    transaction4 = double :transaction, amount: -700, date: '15/01/2023'
-    bank.add_transaction(transaction1)
-    bank.add_transaction(transaction2)
-    bank.add_transaction(transaction3)
-    bank.add_transaction(transaction4)
-    expect(bank.print_statement).to eq "date || credit || debit || balance\n15/01/2023 || || 700.00 || 1800.00\n14/01/2023 || || 500.00 || 2500.00\n13/01/2023 || 2000.00 || || 3000.00\n10/01/2023 || 1000.00 || || 1000.00"
-  end
 end
--- bank/spec/integration_spec.rb
+++ bank/spec/integration_spec.rb
@@ -2,147 +2,159 @@ require 'bank'
 require 'transaction'
 
 RSpec.describe Bank do
-  it 'initializes with an account balance of 0.00' do
-    bank = Bank.new
-    expect(bank.check_balance).to eq 0.00
+  context "when initialized" do
+    it 'has an account balance of 0.00 and an empty transaction array' do
+      bank = Bank.new
+      expect(bank.is_a?(Bank)).to eq true
+      expect(bank.check_balance).to eq 0.00
+      expect(bank.print_statement).to eq 'date || credit || debit || balance'
+    end
   end
 
-  it 'can deposit money and check balance for that deposit value' do
-    bank = Bank.new
-    fake_date = double :date
-    transaction = Transaction.new(1, fake_date)
-    expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
-    bank.add_transaction(transaction)
-    expect(bank.check_balance).to eq 1.00
+  context "when adding a single deposit/withdrawal transaction" do
+    it 'has a positive balance if its a deposit' do
+      bank = Bank.new
+      fake_date = double :date
+      transaction = Transaction.new(1, fake_date)
+      expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
+      bank.add_transaction(transaction)
+      expect(bank.check_balance).to eq 1.00
+    end
+
+    it 'has a balance less than the initial deposit value' do
+      bank = Bank.new
+      fake_date = double :date
+      transaction1 = Transaction.new(10, fake_date)
+      expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
+      transaction2 = Transaction.new(-1, fake_date)
+      expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
+      bank.add_transaction(transaction1)
+      bank.add_transaction(transaction2)
+      expect(bank.check_balance).to eq 9.00
+    end
   end
 
-  it 'can withdraw money and check balance for that withdrawn value' do
-    bank = Bank.new
-    fake_date = double :date
-    transaction1 = Transaction.new(10, fake_date)
-    expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
-    transaction2 = Transaction.new(-1, fake_date)
-    expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
-    bank.add_transaction(transaction1)
-    bank.add_transaction(transaction2)
-    expect(bank.check_balance).to eq 9.00
+  context "several deposits / withdrawals can be made and the balance is checked" do
+    it 'can calculate the balance after several deposits' do
+      bank = Bank.new
+      fake_date = double :date
+      transaction1 = Transaction.new(1, fake_date)
+      expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
+      transaction2 = Transaction.new(2, fake_date)
+      expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
+      transaction3 = Transaction.new(9, fake_date)
+      expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
+      bank.add_transaction(transaction1)
+      bank.add_transaction(transaction2)
+      bank.add_transaction(transaction3)
+      expect(bank.check_balance).to eq 12.00
+    end
+
+    it 'can calculate the balance after several withdrawals' do
+      bank = Bank.new
+      fake_date = double :date
+      transaction1 = Transaction.new(1, fake_date)
+      expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
+      transaction2 = Transaction.new(2, fake_date)
+      expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
+      transaction3 = Transaction.new(9, fake_date)
+      expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
+      transaction4 = Transaction.new(-1, fake_date)
+      expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
+      transaction5 = Transaction.new(-2, fake_date)
+      expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
+      transaction6 = Transaction.new(-5, fake_date)
+      expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
+      bank.add_transaction(transaction1)
+      bank.add_transaction(transaction2)
+      bank.add_transaction(transaction3)
+      bank.add_transaction(transaction4)
+      bank.add_transaction(transaction5)
+      bank.add_transaction(transaction6)
+      expect(bank.check_balance).to eq 4.00
+    end
+
+    it 'can calculate the balance after the example transaction sequence' do
+      bank = Bank.new
+      fake_date = double :date
+      transaction1 = Transaction.new(1000, fake_date)
+      expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
+      transaction2 = Transaction.new(2000, fake_date)
+      expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
+      transaction3 = Transaction.new(-500, fake_date)
+      expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
+      bank.add_transaction(transaction1)
+      bank.add_transaction(transaction2)
+      bank.add_transaction(transaction3)
+      expect(bank.check_balance).to eq 2500.00
+    end
   end
 
-  it 'can deposit money several times and check balance for total balance value' do
-    bank = Bank.new
-    fake_date = double :date
-    transaction1 = Transaction.new(1, fake_date)
-    expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
-    transaction2 = Transaction.new(2, fake_date)
-    expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
-    transaction3 = Transaction.new(9, fake_date)
-    expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
-    bank.add_transaction(transaction1)
-    bank.add_transaction(transaction2)
-    bank.add_transaction(transaction3)
-    expect(bank.check_balance).to eq 12.00
+  context "printing a statement after a single deposit / withdrawal" do
+    it 'prints after a single deposit' do
+      bank = Bank.new
+      fake_date = double :date
+      transaction = Transaction.new(1, fake_date)
+      expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
+      bank.add_transaction(transaction)
+      expect(bank.print_statement).to eq "date || credit || debit || balance\n23/01/2023 || 1.00 || || 1.00"
+    end
+
+    it 'prints after a single withdrawal' do
+      bank = Bank.new
+      fake_date = double :date
+      transaction1 = Transaction.new(10, fake_date)
+      expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
+      transaction2 = Transaction.new(-1, fake_date)
+      expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
+      bank.add_transaction(transaction1)
+      bank.add_transaction(transaction2)
+      expect(bank.print_statement).to eq "date || credit || debit || balance\n23/01/2023 || || 1.00 || 9.00\n23/01/2023 || 10.00 || || 10.00"
+    end
   end
 
-  it 'can withdraw money several times and check balance for total balance value' do
-    bank = Bank.new
-    fake_date = double :date
-    transaction1 = Transaction.new(1, fake_date)
-    expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
-    transaction2 = Transaction.new(2, fake_date)
-    expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
-    transaction3 = Transaction.new(9, fake_date)
-    expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
-    transaction4 = Transaction.new(-1, fake_date)
-    expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
-    transaction5 = Transaction.new(-2, fake_date)
-    expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
-    transaction6 = Transaction.new(-5, fake_date)
-    expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
-    bank.add_transaction(transaction1)
-    bank.add_transaction(transaction2)
-    bank.add_transaction(transaction3)
-    bank.add_transaction(transaction4)
-    bank.add_transaction(transaction5)
-    bank.add_transaction(transaction6)
-    expect(bank.check_balance).to eq 4.00
-  end
+  context "several deposits / withdrawals can be made and the statement printed" do
+    it 'prints after a multiple deposits' do
+      bank = Bank.new
+      fake_date = double :date
+      transaction1 = Transaction.new(1, fake_date)
+      expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
+      transaction2 = Transaction.new(2, fake_date)
+      expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
+      bank.add_transaction(transaction1)
+      bank.add_transaction(transaction2)
+      expect(bank.print_statement).to eq "date || credit || debit || balance\n23/01/2023 || 2.00 || || 3.00\n23/01/2023 || 1.00 || || 1.00"
+    end
 
-  it 'can deposit and withdraw money and check balance for the total balance values' do
-    bank = Bank.new
-    fake_date = double :date
-    transaction1 = Transaction.new(1000, fake_date)
-    expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
-    transaction2 = Transaction.new(2000, fake_date)
-    expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
-    transaction3 = Transaction.new(-500, fake_date)
-    expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
-    bank.add_transaction(transaction1)
-    bank.add_transaction(transaction2)
-    bank.add_transaction(transaction3)
-    expect(bank.check_balance).to eq 2500.00
-  end
+    it 'prints after a multiple withdrawals' do
+      bank = Bank.new
+      fake_date = double :date
+      transaction1 = Transaction.new(45, fake_date)
+      expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
+      transaction2 = Transaction.new(-10, fake_date)
+      expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
+      transaction3 = Transaction.new(-25, fake_date)
+      expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
+      bank.add_transaction(transaction1)
+      bank.add_transaction(transaction2)
+      bank.add_transaction(transaction3)
+      expect(bank.print_statement).to eq "date || credit || debit || balance\n23/01/2023 || || 25.00 || 10.00\n23/01/2023 || || 10.00 || 35.00\n23/01/2023 || 45.00 || || 45.00"
+    end
 
-  it 'can print a statement after a single deposit' do
-    bank = Bank.new
-    fake_date = double :date
-    transaction = Transaction.new(1, fake_date)
-    expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
-    bank.add_transaction(transaction)
-    expect(bank.print_statement).to eq "date || credit || debit || balance\n23/01/2023 || 1.00 || || 1.00"
-  end
-
-  it 'can print a statement after a single withdrawl' do
-    bank = Bank.new
-    fake_date = double :date
-    transaction1 = Transaction.new(10, fake_date)
-    expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
-    transaction2 = Transaction.new(-1, fake_date)
-    expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
-    bank.add_transaction(transaction1)
-    bank.add_transaction(transaction2)
-    expect(bank.print_statement).to eq "date || credit || debit || balance\n23/01/2023 || || 1.00 || 9.00\n23/01/2023 || 10.00 || || 10.00"
-  end
-
-  it 'can print a statement after a multiple deposits' do
-    bank = Bank.new
-    fake_date = double :date
-    transaction1 = Transaction.new(1, fake_date)
-    expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
-    transaction2 = Transaction.new(2, fake_date)
-    expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
-    bank.add_transaction(transaction1)
-    bank.add_transaction(transaction2)
-    expect(bank.print_statement).to eq "date || credit || debit || balance\n23/01/2023 || 2.00 || || 3.00\n23/01/2023 || 1.00 || || 1.00"
-  end
-
-  it 'can print a statement after a multiple withdrawls' do
-    bank = Bank.new
-    fake_date = double :date
-    transaction1 = Transaction.new(45, fake_date)
-    expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
-    transaction2 = Transaction.new(-10, fake_date)
-    expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
-    transaction3 = Transaction.new(-25, fake_date)
-    expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("23/01/2023")
-    bank.add_transaction(transaction1)
-    bank.add_transaction(transaction2)
-    bank.add_transaction(transaction3)
-    expect(bank.print_statement).to eq "date || credit || debit || balance\n23/01/2023 || || 25.00 || 10.00\n23/01/2023 || || 10.00 || 35.00\n23/01/2023 || 45.00 || || 45.00"
-  end
-
-  it 'can print a statement after deposits and withdrawls' do
-    bank = Bank.new
-    fake_date = double :date
-    transaction1 = Transaction.new(1000, fake_date)
-    expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("10/01/2023")
-    transaction2 = Transaction.new(2000, fake_date)
-    expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("13/01/2023")
-    transaction3 = Transaction.new(-500, fake_date)
-    expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("14/01/2023")
-    bank.add_transaction(transaction1)
-    bank.add_transaction(transaction2)
-    bank.add_transaction(transaction3)
-    expect(bank.print_statement).to eq "date || credit || debit || balance\n14/01/2023 || || 500.00 || 2500.00\n13/01/2023 || 2000.00 || || 3000.00\n10/01/2023 || 1000.00 || || 1000.00"
+    it 'prints the example transaction sequence' do
+      bank = Bank.new
+      fake_date = double :date
+      transaction1 = Transaction.new(1000, fake_date)
+      expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("10/01/2023")
+      transaction2 = Transaction.new(2000, fake_date)
+      expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("13/01/2023")
+      transaction3 = Transaction.new(-500, fake_date)
+      expect(fake_date).to receive(:strftime).with("%d/%m/%Y").and_return("14/01/2023")
+      bank.add_transaction(transaction1)
+      bank.add_transaction(transaction2)
+      bank.add_transaction(transaction3)
+      expect(bank.print_statement).to eq "date || credit || debit || balance\n14/01/2023 || || 500.00 || 2500.00\n13/01/2023 || 2000.00 || || 3000.00\n10/01/2023 || 1000.00 || || 1000.00"
+    end
   end
 
   it 'raises error if there are insufficient funds' do
@@ -154,4 +166,5 @@ RSpec.describe Bank do
     transaction2 = Transaction.new(-100, fake_date)
     expect { bank.add_transaction(transaction2) }.to raise_error 'Insufficienet funds'
   end
+
 end
